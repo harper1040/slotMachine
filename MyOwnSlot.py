4c085cb4bf71802406858922e5366c76989f7060
harper1040 <75588870+harper1040@users.noreply.github.com>
2024-09-09 03:08:35 -0400

    Updated some cosmetics

--- MyOwnSlot.py
+++ MyOwnSlot.py
@@ -7,91 +7,141 @@ Slot Machine
 import random
 import time
 
+#This is a class for various escapement values
+class escape:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+   CLEAR ='\x1b[2K'
+   UP ='\033[1A'
+
 bankRoll = 100
 
-
+#Game loop asks if you want to keep play then loops you back into the game.
 def gameLoop():
-    leave = input("Would you like to play again? Y/N ").upper()  
-    if leave == "Y":
+    global bankRoll
+    if bankRoll > 50:
+        leave = input("\nWould you like to play again? Y/N ").upper() 
+    elif bankRoll <= 50:
+        G = escape.BLUE + "G" + escape.END
+        A = escape.CYAN + "A" + escape.END
+        Q = escape.PURPLE + "Q" + escape.END
+        leave = input(f"""
+        Having a rough time huh? You can choose to go on or to add funds.
+        ({G})o On or
+        ({A})dd Funds or
+        ({Q})uit if your ready to leave
+        """).upper()
+    if leave == "Y" or leave == "G":
         placeBets()
-    elif leave == "N":
-        print("\nThanks for Play Come Back Again!!")
+    elif leave == "N" or leave == "Q":
+        bankRollC = escape.GREEN + escape.UNDERLINE + escape.BOLD + str(bankRoll) + escape.END
+        print(f"\nHere's your money, I guess. ${bankRollC} At least you have some left!")
+        print("\nThanks for Playing, Come Back Again!!")
+    elif leave == "A":
+        bankRoll = bankRun(bankRoll)
+        bankRollC = escape.RED + escape.BOLD + escape.UNDERLINE + str(bankRoll) + escape.END
+        print(f"Your current bank roll is ${bankRollC} good luck!!\n")
+        gameLoop()
     else:
         print("\nInvalid Choice It's not that hard")
         gameLoop()
 
+#Bank run allows a player to "purchase" more money to play with and is propted to the player when their money is below $50. There is a set amount you can add 10-200, if you make and invalid entry you get nothing.
 def bankRun(bankInfo):
-    runToBank = input("Would you like to add funds? Y/N ").upper()
-    if runToBank == "Y":
-        amount = input("\nHow much would you like to add? Choose between 10 and 200: ")
-        if int(amount) < 10 and int(amount) > 200:
-            print("\nInvalid entry no money for you!")
-            return bankInfo
-        else:
-            print("\nPlease provide me with your banking information!")
-            time.sleep(2)
-            print("\nJust kinding here's your \"cash\"\n")
-            bankInfo += int(amount)
-            return bankInfo
+    amount = input("\nHow much would you like to add? Choose between 10 and 200: ")
+    dumb = escape.BOLD + escape.RED +"DUMBY" + escape.END
+    try:
+        int(amount)
+    except ValueError:
+        print(f"\nHey that's not a number {dumb}!\n")
+        return bankInfo
+    if amount >= 10 and int(amount) <= 200:
+        print("\nPlease provide me with your banking information!\n")
+        time.sleep(2)
+        print("Just kinding here's your \"cash\"\n")
+        bankInfo += int(amount)
+        return bankInfo
     else:
+        print("Invalid entry no money for you!\n")
         return bankInfo
 
-def placeBets():
-    global bankRoll
-    
-    bet = 5
 
+#This is the main game that rolls random numbers and associates those with the reel items.
+def placeBets():
+    global bankRoll  
+    bet = 5
+    #These are all the various variables :D.
     melon = 0
     seven = 0
     bar = 0
     lemon = 0
     cherry = 0
+    reelFill = ["melon", "seven", "bar", "lemon", "cherry"]
+    #These are styling for the reel items so I can add some escape to the console
+    melonStyle = escape.GREEN + "(" + escape.END + escape.CYAN + "M" + escape.END + escape.GREEN + ")" + escape.END
+    sevenStyle = "[" + escape.RED + escape.BOLD + "7" + escape.END + "]"
+    barStyle = escape.BLUE + "---" + escape.END
+    cherryStyle = escape.RED + escape.BOLD + "o" + escape.END + escape.GREEN + "^" + escape.END + escape.RED + escape.BOLD + "o" + escape.END
+    lemonStyle = escape.YELLOW + escape.BOLD + "{7}" + escape.END
+    
+    reelFill2 = [melonStyle, sevenStyle, barStyle, lemonStyle, cherryStyle]
+    winReel = []
 
+#These are all payout values
     melonPay = 3
     sevenPay = 12
     barPay = 5
     cherryPay = 7
 
-    #melon, 7, bar, lemon, cherry
     winnings = 0
-
-    print("Spinning Sots...")
-    time.sleep(1)
     
     bankRoll -= bet
+    
+    #This bit picks a random number 3 times and adds 1 to the chosen items variable for use in payout calculation.
     for i in range(1,4):
         slot1 = random.randint(1,1000)
-        if slot1 >= 1 and slot1 <= 175:
+        if slot1 >= 1 and slot1 <= 150:
             melon += 1
-            print("Melon")
+            winReel.append(reelFill[0])
         elif slot1 >= 900:
             seven += 1
-            print("7")
+            winReel.append(reelFill[1])
         elif slot1 >= 800 and slot1 <= 899:
             bar += 1
-            print("bar")
-        elif slot1 >= 325 and slot1 <= 500:
+            winReel.append(reelFill[2])
+        elif slot1 >= 350 and slot1 <= 500:
             lemon += 1
-            print("lemon")
-        elif slot1 >= 501 and slot1 <= 799:
+            winReel.append(reelFill[3])
+        elif slot1 >= 501 and slot1 <= 699:
             cherry += 1
-            print("cherry")
+            winReel.append(reelFill[4])
         else:
-            print("gap")
-        time.sleep(.5)
+            winReel.append("Gap")
+        
+    #This bit spins your reels and prints out the final reel based on the out put from above.
+    print("\n")
+    counter = 0
+    num = .15
+    for i in range(6):
+        if counter == 5:
+            num = .6
+        for i in range(5):
+            print(reelFill2[i], reelFill2[-i], reelFill2[i])
+            time.sleep(num)
+            print(escape.UP, end=escape.CLEAR)
+        counter += 1
+        
+    print("\n" + winReel[0], winReel[1], winReel[2] + "\n")
 
-    """for i in roll:
-        if i == "Melon":
-            melon += 1
-        elif i == "7":
-            seven += 1
-        elif i == "Bar":
-            bar += 1
-        elif i == "Lemon":
-            lemon += 1
-        elif i == "Cherry":
-            cherry += 1"""
-
+#Here we check for winning payouts atm it's just 2 or more gets multiplied by an arbitrary item value.
     if melon >= 2:
         winnings = melon * melonPay
     elif seven >= 2:
@@ -103,19 +153,25 @@ def placeBets():
     elif cherry >= 2:
         winnings = cherry * cherryPay
         
-
+#Then add them to the bankroll.
     bankRoll +=winnings
 
+#This is your winning information message either you won or you didn't. If you did you get told how much if not it mocks you Willy Wonka style.
+    nothing = escape.RED + escape.BOLD + "NOTHING!!!" + escape.END
     if winnings == 0:
-        print("\nYou won NOTHING!! You get NOTHING!!!")
-    else:
-        print(f"\nYou won ${winnings}!!")
-    print(f"You now have ${bankRoll}\n")
-    #print(melon, seven, bar, lemon, cherry)
-    
-    if bankRoll <= 90:
-        bankRoll = bankRun(bankRoll)
-        print(f"Your current bank roll is {bankRoll} good luck!!\n")
+        print(f"You won {nothing} You get {nothing}\n")
+    elif winnings > 0:
+        winC = escape.GREEN + escape.BOLD + escape.UNDERLINE + str(winnings) + escape.END
+        winC = '\033[1m' + '\033[4m' + winC + '\033[0m'
+        print(f"You won ${winC}!!\n")
+    bankRollC = escape.RED + escape.BOLD + escape.UNDERLINE + str(bankRoll) + escape.END
+    print(f"You now have ${bankRollC}\n")
     gameLoop()
+    #print(melon, seven, bar, lemon, cherry)
 
+#This bit cheks if your bank drops to 50 and prompts you the option to add funds.  
+    if bankRoll < bet:
+        print("\nSorry Loser you're outa Cash!!")
+    
+#Of course this guy starts us off by immediately running the game.
 placeBets()
